9c85e8906d5d43d8c3a06498b353adcd3c4de260
minh <Lminhphu0304@gmail.com>
2023-03-11 15:04:55 +1300

    LOL

---
+++ Level1/js/item.js
@@ -0,0 +1,45 @@
+const itemToBuild = [
+  {
+    name: "new hot tub",
+    price: 24000,
+  },
+  {
+    name: "new hot tub",
+    price: 24000,
+  },
+  {
+    name: "new hot tub",
+    price: 24000,
+  },
+  {
+    name: "new hot tub",
+    price: 24000,
+  },
+];
+
+// function itemRender() {
+//   const purchaseDiv = document.getElementsByClassName("purchase").item(0)
+//   for (const item of itemToBuild) {
+//     const div = document.createElement("div");
+//     const name = document.createElement("p");
+//     name.innerText = `Name ${item.name}`;
+//     const price = document.createElement("p");
+//     price.innerText = `Price: $${item.price}`;
+//     div.appendChild(name);
+//     div.appendChild(price);
+//     purchaseDiv.appendChild(div);
+//   }
+// }
+// itemRender()
+
+let totalDebt = 0
+
+function renderDebt() {
+  const item = document.getElementsByClassName('status').item(0)
+  const div = document.createElement('div')
+  const text = document.createElement('p')
+  text.innerText = `Total Debt: $${totalDebt}`
+  div.appendChild(text)
+  item.appendChild(div)
+}
+renderDebt()
