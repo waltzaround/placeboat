54560a296cde321aef8a3da762d2146ad6acb088
minh <Lminhphu0304@gmail.com>
2023-03-11 16:35:50 +1300

    YOLO

--- Level1/js/item.js
+++ Level1/js/item.js
@@ -46,30 +46,18 @@ const newAudio = audio.cloneNode()
 newAudio.play()
 }
 
-
-// function itemRender() {
-//   const purchaseDiv = document.getElementsByClassName("purchase").item(0)
-//   for (const item of itemToBuild) {
-//     const div = document.createElement("div");
-//     const name = document.createElement("p");
-//     name.innerText = `Name ${item.name}`;
-//     const price = document.createElement("p");
-//     price.innerText = `Price: $${item.price}`;
-//     div.appendChild(name);
-//     div.appendChild(price);
-//     purchaseDiv.appendChild(div);
-//   }
-// }
-// itemRender()
-
 let totalDebt = 0;
 let eventIndex = Math.floor(Math.random() * events.length);
+
+function formatNumber(number) {
+  return number.toLocaleString()
+}
 
 function renderDebt() {
   const item = document.getElementsByClassName('status').item(0)
   const div = document.createElement('div')
   const text = document.createElement('h3')
-  text.innerText = `Total Debt: $${totalDebt}`
+  text.innerText = `Total Debt: $${formatNumber(totalDebt)}`
   div.innerHTML = text.innerHTML
   item.innerHTML = div.innerHTML
 }
